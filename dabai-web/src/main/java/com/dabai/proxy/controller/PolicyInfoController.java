4bd21d9423b019c3976e3386899e1e4b4038be48
zhujinshan <zhujinshan@popmart.com>
2022-05-15 14:32:40 +0800

    接口定义

--- dabai-web/src/main/java/com/dabai/proxy/controller/PolicyInfoController.java
+++ dabai-web/src/main/java/com/dabai/proxy/controller/PolicyInfoController.java
@@ -3,6 +3,7 @@ package com.dabai.proxy.controller;
 import com.dabai.proxy.config.result.Result;
 import com.dabai.proxy.config.token.CheckToken;
 import com.dabai.proxy.req.PolicyInfoPageReq;
+import com.dabai.proxy.resp.PolicyInfoResp;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +25,7 @@ public class PolicyInfoController {
     @PostMapping("/pageQuery")
     @CheckToken
     @ApiOperation(value = "同步微信用户基本信息", httpMethod = "POST")
-    public Result<Object> pageQuery(@RequestBody PolicyInfoPageReq policyInfoPageReq) {
+    public Result<PolicyInfoResp> pageQuery(@RequestBody PolicyInfoPageReq policyInfoPageReq) {
         return null;
     }
 }
